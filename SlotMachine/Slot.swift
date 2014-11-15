72e9ffdc8397a5d3e8a6826435d6207119808772
David Grevink <dgrevink@starplace.org>
2014-11-14 22:49:04 -0500

    UI 50% done
    - added 4 container
    - added cards containers
    - added labels & buttons
    - added app icon

---
+++ SlotMachine/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SlotMachine
+//
+//  Created by David Grevink on 2014-11-14.
+//  Copyright (c) 2014 David Grevink. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct slot {
+    var value = 0
+    var image = UIImage(named: "Ace")
+    var isRed = true
+}
